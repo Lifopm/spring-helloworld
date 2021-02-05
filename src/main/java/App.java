23f95f608c380a5a08b2ebac740129c9061511e1
Nikaria <cogat@mail.ru>
2021-02-06 02:42:01 +0300

    2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean3 == bean4);
 
     }
 }
