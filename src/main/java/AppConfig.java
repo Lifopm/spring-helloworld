f624e388bc948bce6ba65411551d763b09d674ad
Nikaria <cogat@mail.ru>
2021-02-06 02:20:38 +0300

    1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,4 +1,3 @@
-import Cat.Cat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
